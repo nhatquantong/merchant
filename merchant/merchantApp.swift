3b4d6124ff06a24d6c6000f219950f8eea9f5340
Quan Tong <61928209+nhatquantong@users.noreply.github.com>
2023-09-14 19:16:03 +0700

    Initial Commit

---
+++ merchant/merchantApp.swift
@@ -0,0 +1,17 @@
+//
+//  merchantApp.swift
+//  merchant
+//
+//  Created by Nhật Quân on 14/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct merchantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ merchantUITests/merchantUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  merchantUITestsLaunchTests.swift
+//  merchantUITests
+//
+//  Created by Nhật Quân on 14/09/2023.
+//
+
+import XCTest
+
+final class merchantUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
